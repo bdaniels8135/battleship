6ff11e31e8520a8271f38a71100df4931a69158d
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-10 18:23:43 -0500

    Create Game class

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,58 @@
-class Game {}
+const Gameboard = require("./Gameboard");
+const Player = require("./Player");
+
+class Game {
+  #playerOne;
+
+  #playerTwo;
+
+  #currentPlayer;
+
+  constructor(playerOneName, playerTwoName) {
+    this.#playerOne = new Player(playerOneName);
+    this.#playerTwo = new Player(playerTwoName);
+    this.#currentPlayer = this.#playerOne;
+    const fleetCoords = [
+      [
+        [0, 0],
+        [0, 1],
+      ],
+      [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+      ],
+      [
+        [2, 0],
+        [2, 1],
+        [2, 2],
+      ],
+      [
+        [3, 0],
+        [3, 1],
+        [3, 2],
+        [3, 3],
+      ],
+      [
+        [4, 0],
+        [4, 1],
+        [4, 2],
+        [4, 3],
+        [4, 4],
+      ],
+    ];
+    this.#playerOne.gb = new Gameboard(fleetCoords);
+    this.#playerTwo.gb = new Gameboard(fleetCoords);
+  }
+
+  playRound(attackCoords) {
+    const attackReport = this.#currentPlayer.gb.receiveAttack(attackCoords);
+    return attackReport;
+  }
+
+  get currentPlayer() {
+    return this.#currentPlayer.name;
+  }
+}
 
 module.exports = Game;
--- src/Game.test.js
+++ src/Game.test.js
@@ -1,3 +1,17 @@
 const Game = require("./Game");
 
-test("", () => {});
+let game;
+
+beforeEach(() => {
+  game = new Game("Bob", "Donna");
+});
+
+test("playRound returns the attack report data", () => {
+  const roundReport = game.playRound([0, 0]);
+  expect(roundReport.isAHit).toBe(true);
+  expect(roundReport.isShipSunk).toBe(false);
+});
+
+test("returns current player name", () => {
+  expect(game.currentPlayer).toBe("Bob");
+});

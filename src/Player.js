f4cc5da4b727846907160da7c0e53e5c536b0a6b
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-10 16:31:01 -0500

    Implement players constructed without names logic
    Players created without names are bots called Skynet

--- src/Player.js
+++ src/Player.js
@@ -1,12 +1,19 @@
 class Player {
   #name;
 
+  #isAI;
+
   constructor(name) {
-    this.#name = name;
+    this.#name = name || "Skynet";
+    this.#isAI = !name;
   }
 
   get name() {
     return this.#name;
+  }
+
+  get isAI() {
+    return this.#isAI;
   }
 }
 
--- src/Player.test.js
+++ src/Player.test.js
@@ -4,3 +4,13 @@ test("players have a name", () => {
   const bob = new Player("Bob");
   expect(bob.name).toBe("Bob");
 });
+
+test("players created without a name are called Skynet", () => {
+  const skynet = new Player();
+  expect(skynet.name).toBe("Skynet");
+});
+
+test("players created without a name are bots", () => {
+  const skynet = new Player();
+  expect(skynet.isAI).toBe(true);
+});

0c91e07b73697627d53ba94312b024e7e679eeba
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-09 22:58:45 -0500

    Implement and test ship class functionality

--- src/Ship.js
+++ src/Ship.js
@@ -1,1 +1,24 @@
-export default class Ship {}
+class Ship {
+  #length;
+
+  #hits;
+
+  constructor(length) {
+    this.#length = length;
+    this.#hits = 0;
+  }
+
+  get length() {
+    return this.#length;
+  }
+
+  hit() {
+    if (!this.isSunk()) this.#hits += 1;
+  }
+
+  isSunk() {
+    return this.#hits >= this.#length;
+  }
+}
+
+module.exports = Ship;
--- src/Ship.test.js
+++ src/Ship.test.js
@@ -1,1 +1,16 @@
-import Ship from "./Ship";
+const Ship = require("./Ship");
+
+test("sets length correctly", () => {
+  const shipLengthThree = new Ship(3);
+  expect(shipLengthThree.length).toBe(3);
+});
+
+test("only sinks when hit a number of times equal to length", () => {
+  const shipLengthThree = new Ship(3);
+  shipLengthThree.hit();
+  expect(shipLengthThree.isSunk()).toBe(false);
+  shipLengthThree.hit();
+  expect(shipLengthThree.isSunk()).toBe(false);
+  shipLengthThree.hit();
+  expect(shipLengthThree.isSunk()).toBe(true);
+});

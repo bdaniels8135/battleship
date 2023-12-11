4fb3233f8569b359fc900242776cca008936b6f7
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-10 23:02:38 -0500

    Add labels to inputs and selects

--- src/views/gameStartView.js
+++ src/views/gameStartView.js
@@ -2,6 +2,7 @@ const {
   buildSelectOption,
   buildInputHtml,
   wrapHtmlElements,
+  buildLabelHtml,
 } = require("./htmlBuilder");
 
 function gameStartView() {
@@ -12,20 +13,38 @@ function gameStartView() {
     humanPlayerOption,
     computerPlayerOption
   );
+  playerOneSelect.id = "player-one-select";
+  const playerOneSelectLabel = buildLabelHtml(
+    "player-one-select",
+    "Player One Type:"
+  );
   const playerTwoSelect = wrapHtmlElements(
     "select",
     humanPlayerOption.cloneNode(true),
     computerPlayerOption.cloneNode(true)
+  );
+  playerTwoSelect.id = "player-two-select";
+  const playerTwoSelectLabel = buildLabelHtml(
+    "player-two-select",
+    "Player Two Type:"
   );
   const playerOneNameInput = buildInputHtml(
     "text",
     "player-one-name",
     "player-one-name"
   );
+  const playerOneNameInputLabel = buildLabelHtml(
+    "player-one-name",
+    "Player One Name:"
+  );
   const playerTwoNameInput = buildInputHtml(
     "text",
     "player-two-name",
     "player-two-name"
+  );
+  const playerTwoNameInputLabel = buildLabelHtml(
+    "player-two-name",
+    "Player Two Name:"
   );
   const startGameBtn = buildInputHtml(
     "button",
@@ -35,9 +54,13 @@ function gameStartView() {
   startGameBtn.value = "Start Game";
   const html = wrapHtmlElements(
     "form",
+    playerOneSelectLabel,
     playerOneSelect,
+    playerOneNameInputLabel,
     playerOneNameInput,
+    playerTwoSelectLabel,
     playerTwoSelect,
+    playerTwoNameInputLabel,
     playerTwoNameInput,
     startGameBtn
   );

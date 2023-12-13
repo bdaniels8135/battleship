66ac972b9a15414424ba2bded7288d7566865f4f
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-12 23:39:25 -0500

    Refactor to use div instead of main

--- src/views/playRoundView.js
+++ src/views/playRoundView.js
@@ -24,7 +24,7 @@ export default function playRoundView() {
   resetBtn.value = "Resign/New Game";
 
   const html = wrapHtmlElements(
-    "main",
+    "div",
     playerOneGBText,
     playerOneGBHtml,
     announcementTextBox,

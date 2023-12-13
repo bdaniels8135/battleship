6744acfa43c10250d3d5aaeb562eed9acaae94c1
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-13 00:08:30 -0500

    Add player name input display toggling

--- src/views/ViewController.js
+++ src/views/ViewController.js
@@ -1,6 +1,7 @@
 import playRoundView from "./playRoundView";
 import gameStartView from "./gameStartView";
 import { buildPageHeader } from "./htmlBuilder";
+import "./index.css";
 
 export default function ViewController() {
   const body = document.querySelector("body");
@@ -12,10 +13,41 @@ export default function ViewController() {
     main.innerHTML = "";
   }
 
-  function displayGameStartView() {
+  function displayGameStartView(startGameFunc) {
     clearMain();
     const gsv = gameStartView();
     main.appendChild(gsv);
+
+    const playerOneSelect = document.querySelector("#player-one-select");
+    const playerOneNameInput = document.querySelector("#player-one-name");
+    playerOneSelect.addEventListener("change", () => {
+      playerOneNameInput.classList.remove("displayed");
+      playerOneNameInput.value = "";
+      if (playerOneSelect.value === "Human")
+        playerOneNameInput.classList.add("displayed");
+    });
+
+    const playerTwoSelect = document.querySelector("#player-two-select");
+    const playerTwoNameInput = document.querySelector("#player-two-name");
+    playerTwoSelect.addEventListener("change", () => {
+      playerTwoNameInput.classList.remove("displayed");
+      playerTwoNameInput.value = "";
+      if (playerTwoSelect.value === "Human")
+        playerTwoNameInput.classList.add("displayed");
+    });
+
+    const startGameBtn = document.querySelector("#start-game-button");
+    startGameBtn.addEventListener("click", () => {
+      if (playerOneSelect.value !== "" && playerTwoSelect.value !== "") {
+        startGameFunc({
+          playerOneName: playerOneNameInput.value,
+          playerTwoName: playerTwoNameInput.value,
+          playerOneType: playerOneSelect.value,
+          playerTwoType: playerTwoSelect.value,
+        });
+      }
+      // Display form validation text?
+    });
   }
 
   function displayPlayRoundView() {

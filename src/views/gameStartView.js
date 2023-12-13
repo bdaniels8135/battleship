33dafbe2ef7eec216bbbee201e2a6df68dbad7bd
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-12 22:54:26 -0500

    Refactor gameStartView to use placeholders

--- src/views/gameStartView.js
+++ src/views/gameStartView.js
@@ -2,51 +2,46 @@ import {
   buildSelectOption,
   buildInputHtml,
   wrapHtmlElements,
-  buildLabelHtml,
 } from "./htmlBuilder";
 import "./gameStartView.css";
 
 export default function gameStartView() {
+  const playerOneDefaultOption = buildSelectOption(
+    "-- Select Player One Type --",
+    ""
+  );
+  const playerTwoDefaultOption = buildSelectOption(
+    "-- Select Player Two Type --",
+    ""
+  );
   const humanPlayerOption = buildSelectOption("Human", "Human");
   const computerPlayerOption = buildSelectOption("Computer", "Computer");
   const playerOneSelect = wrapHtmlElements(
     "select",
+    playerOneDefaultOption,
     humanPlayerOption,
     computerPlayerOption
   );
   playerOneSelect.id = "player-one-select";
-  const playerOneSelectLabel = buildLabelHtml(
-    "player-one-select",
-    "Player One Type:"
-  );
   const playerTwoSelect = wrapHtmlElements(
     "select",
+    playerTwoDefaultOption,
     humanPlayerOption.cloneNode(true),
     computerPlayerOption.cloneNode(true)
   );
   playerTwoSelect.id = "player-two-select";
-  const playerTwoSelectLabel = buildLabelHtml(
-    "player-two-select",
-    "Player Two Type:"
-  );
   const playerOneNameInput = buildInputHtml(
     "text",
     "player-one-name",
     "player-one-name"
   );
-  const playerOneNameInputLabel = buildLabelHtml(
-    "player-one-name",
-    "Player One Name:"
-  );
+  playerOneNameInput.placeholder = "Enter Player One's Name";
   const playerTwoNameInput = buildInputHtml(
     "text",
     "player-two-name",
     "player-two-name"
   );
-  const playerTwoNameInputLabel = buildLabelHtml(
-    "player-two-name",
-    "Player Two Name:"
-  );
+  playerTwoNameInput.placeholder = "Enter Player Two's Name";
   const startGameBtn = buildInputHtml(
     "button",
     "start-game-button",
@@ -55,13 +50,9 @@ export default function gameStartView() {
   startGameBtn.value = "Start Game";
   const html = wrapHtmlElements(
     "form",
-    playerOneSelectLabel,
     playerOneSelect,
-    playerOneNameInputLabel,
     playerOneNameInput,
-    playerTwoSelectLabel,
     playerTwoSelect,
-    playerTwoNameInputLabel,
     playerTwoNameInput,
     startGameBtn
   );

cff2558b0da319bc8ca9f7d12c6129262824e105
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-14 17:09:59 -0500

    Adjust playRoundView namespace

--- src/views/playRoundView.js
+++ src/views/playRoundView.js
@@ -7,29 +7,35 @@ import {
 import "./playRoundView.css";
 
 export default function playRoundView() {
-  const playerOneGBText = buildTextHtml("p", "Your Gameboard");
-  playerOneGBText.id = "player-one-gb-text";
-  const playerTwoGBText = buildTextHtml("p", "Opponent's Gameboard");
-  playerTwoGBText.id = "player-two-gb-text";
+  const currentPlayerGBLabel = buildTextHtml("p", "Your Gameboard");
+  currentPlayerGBLabel.id = "current-player-gb-label";
+  currentPlayerGBLabel.classList.add("gb-label");
 
-  const playerOneGBHtml = buildGameboardHtml();
-  playerOneGBHtml.id = "player-one-gb";
-  const playerTwoGBHtml = buildGameboardHtml();
-  playerTwoGBHtml.id = "player-two-gb";
+  const opponentPlayerGBLabel = buildTextHtml("p", "Opponent's Gameboard");
+  opponentPlayerGBLabel.id = "opponent-player-gb-label";
+  opponentPlayerGBLabel.classList.add("gb-label");
+
+  const currentPlayerGB = buildGameboardHtml();
+  currentPlayerGB.id = "current-player-gb";
+  currentPlayerGB.classList.add("gameboard");
+
+  const opponentPlayerGB = buildGameboardHtml();
+  opponentPlayerGB.id = "opponent-player-gb";
+  opponentPlayerGB.classList.add("gameboard");
 
   const announcementTextBox = buildTextHtml("p", "Announcement Text Box");
   announcementTextBox.id = "announcement-text-box";
 
   const resetBtn = buildInputHtml("button", "reset-btn", "reset-btn");
-  resetBtn.value = "Resign/New Game";
+  resetBtn.value = "Resign Game";
 
   const html = wrapHtmlElements(
     "div",
-    playerOneGBText,
-    playerOneGBHtml,
+    currentPlayerGBLabel,
+    currentPlayerGB,
     announcementTextBox,
-    playerTwoGBHtml,
-    playerTwoGBText,
+    opponentPlayerGB,
+    opponentPlayerGBLabel,
     resetBtn
   );
   html.id = "game-container";

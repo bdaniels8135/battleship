1cae9c27f7f0a624778de7110ebc88355bd009c0
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-16 00:08:58 -0500

    Refactor gameboard tests

--- src/gameLogic/Gameboard.test.js
+++ src/gameLogic/Gameboard.test.js
@@ -15,96 +15,93 @@ const shipConstructorSpy = (() => {
 
 const Gameboard = require("./Gameboard");
 
-describe("Empty gameboard", () => {
-  test("does not call ship constructor", () => {
-    shipConstructorSpy.mockClear();
-    const gb = new Gameboard();
-    expect(shipConstructorSpy).toHaveBeenCalledTimes(0);
-  });
+const fleetCoords = [
+  [
+    [0, 0],
+    [0, 1],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  [
+    [3, 0],
+    [3, 1],
+    [3, 2],
+    [3, 3],
+  ],
+  [
+    [4, 0],
+    [4, 1],
+    [4, 2],
+    [4, 3],
+    [4, 4],
+  ],
+];
+
+let gb;
+
+beforeEach(() => {
+  shipConstructorSpy.mockClear();
+  gb = new Gameboard(fleetCoords);
 });
 
-describe("Populated gameboard", () => {
-  const fleetCoords = [
-    [
-      [0, 0],
-      [0, 1],
-    ],
-    [
-      [1, 0],
-      [1, 1],
-      [1, 2],
-    ],
-    [
-      [2, 0],
-      [2, 1],
-      [2, 2],
-    ],
-    [
-      [3, 0],
-      [3, 1],
-      [3, 2],
-      [3, 3],
-    ],
-    [
-      [4, 0],
-      [4, 1],
-      [4, 2],
-      [4, 3],
-      [4, 4],
-    ],
-  ];
+test("throws error if no fleet coordinate are passed", () => {
+  function noFleetCoords() {
+    gb = new Gameboard();
+  }
+  expect(noFleetCoords).toThrow("Gameboard requires fleet coordinates");
+});
 
-  let gb = new Gameboard(fleetCoords);
+test("calls ship constructor when passed ship coords", () => {
+  expect(shipConstructorSpy).toHaveBeenCalledTimes(5);
+});
 
-  beforeEach(() => {
-    shipConstructorSpy.mockClear();
-    gb = new Gameboard(fleetCoords);
-  });
+test("returns list of fleet coords correctly", () => {
+  expect(gb.fleetCoords).toEqual(fleetCoords.flat());
+});
 
-  test("calls ship constructor when passed ship coords", () => {
-    expect(shipConstructorSpy).toHaveBeenCalledTimes(5);
-  });
+test("returns isAHit as true when receivedAttack is a hit", () => {
+  expect(gb.receiveAttack([0, 0]).isAHit).toBe(true);
+});
 
-  test("returns isAHit as true when receivedAttack is a hit", () => {
-    expect(gb.receiveAttack([0, 0]).isAHit).toBe(true);
-  });
+test("returns isAHit as false when receivedAttack is a miss", () => {
+  expect(gb.receiveAttack([5, 0]).isAHit).toBe(false);
+});
 
-  test("returns isAHit as false when receivedAttack is a miss", () => {
-    expect(gb.receiveAttack([5, 0]).isAHit).toBe(false);
-  });
+test("returns isShipSunk as true when received attack sinks the ship", () => {
+  gb.receiveAttack([0, 0]);
+  const attackReport = gb.receiveAttack([0, 1]);
+  expect(attackReport.isShipSunk).toBe(true);
+});
 
-  test("throws error if receivedAttack coord is a repeat", () => {
-    function repeatedAttack() {
-      gb.receiveAttack([0, 0]);
-      gb.receiveAttack([0, 0]);
-    }
-    expect(repeatedAttack).toThrow(
-      "Player may not attack the same coordinate twice."
-    );
-  });
+test("returns lists of hit and miss coordinates", () => {
+  gb.receiveAttack([0, 0]);
+  gb.receiveAttack([5, 5]);
+  expect(gb.hitCoords).toEqual([[0, 0]]);
+  expect(gb.missCoords).toEqual([[5, 5]]);
+});
 
-  test("returns isShipSunk as true when received attack sinks the ship", () => {
+test("throws error if receivedAttack coord is a repeat", () => {
+  function repeatedAttack() {
     gb.receiveAttack([0, 0]);
-    const attackReport = gb.receiveAttack([0, 1]);
-    expect(attackReport.isShipSunk).toBe(true);
-  });
-
-  test("fleetIsSunk is true only if all ships are sunk", () => {
-    fleetCoords.flat().forEach((coord) => {
-      expect(gb.fleetIsSunk).toBe(false);
-      gb.receiveAttack(coord);
-    });
-    expect(gb.fleetIsSunk).toBe(true);
-  });
-
-  test("returns lists of hit and miss coordinates", () => {
     gb.receiveAttack([0, 0]);
-    gb.receiveAttack([5, 5]);
-    expect(gb.hitCoords).toEqual([[0, 0]]);
-    expect(gb.missCoords).toEqual([[5, 5]]);
-  });
+  }
+  expect(repeatedAttack).toThrow(
+    "Player may not attack the same coordinate twice."
+  );
+});
 
-  test("returns list of fleet coords correctly", () => {
-    expect(gb.fleetCoords).toEqual(fleetCoords.flat());
+test("fleetIsSunk is true only if all ships are sunk", () => {
+  fleetCoords.flat().forEach((coord) => {
+    expect(gb.fleetIsSunk).toBe(false);
+    gb.receiveAttack(coord);
   });
+  expect(gb.fleetIsSunk).toBe(true);
 });

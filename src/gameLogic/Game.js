e77c0182d8bbb8a4f3724993ff4287f37933b98d
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-16 00:15:28 -0500

    Refactor game class

--- src/gameLogic/Game.js
+++ src/gameLogic/Game.js
@@ -1,6 +1,36 @@
 const Gameboard = require("./Gameboard");
 const Player = require("./Player");
 
+const fleetCoords = [
+  [
+    [0, 0],
+    [0, 1],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  [
+    [3, 0],
+    [3, 1],
+    [3, 2],
+    [3, 3],
+  ],
+  [
+    [4, 0],
+    [4, 1],
+    [4, 2],
+    [4, 3],
+    [4, 4],
+  ],
+];
+
 class Game {
   #playerOne;
 
@@ -13,53 +43,24 @@ class Game {
     this.#playerOne = new Player(playerOneName);
     this.#playerTwo = new Player(playerTwoName);
     this.#currentPlayer = this.#playerOne;
-    const fleetCoords = [
-      [
-        [0, 0],
-        [0, 1],
-      ],
-      [
-        [1, 0],
-        [1, 1],
-        [1, 2],
-      ],
-      [
-        [2, 0],
-        [2, 1],
-        [2, 2],
-      ],
-      [
-        [3, 0],
-        [3, 1],
-        [3, 2],
-        [3, 3],
-      ],
-      [
-        [4, 0],
-        [4, 1],
-        [4, 2],
-        [4, 3],
-        [4, 4],
-      ],
-    ];
     this.#playerOne.gb = new Gameboard(fleetCoords);
     this.#playerTwo.gb = new Gameboard(fleetCoords);
     if (!playerOneName) this.playRound(this.#currentPlayer.getAIMove());
   }
 
-  #switchPlayers() {
+  playRound(attackCoords) {
+    if (this.isOver) throw new Error("Cannot play rounds after game ends");
+
     this.#currentPlayer =
       this.#currentPlayer === this.#playerOne
         ? this.#playerTwo
         : this.#playerOne;
-  }
 
-  playRound(attackCoords) {
-    if (this.isOver) throw new Error("Cannot play rounds after game ends");
-    this.#switchPlayers();
     let attackReport = this.#currentPlayer.gb.receiveAttack(attackCoords);
+
     if (this.#currentPlayer.isAI && !this.isOver)
       attackReport = this.playRound(this.#currentPlayer.getAIMove());
+
     return attackReport;
   }
 

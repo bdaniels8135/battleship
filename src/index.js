acee0c7ccb627d923095d35f58510d93315cf877
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-17 11:45:45 -0500

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ViewController from "./views/ViewController";
 
 const VC = ViewController();
 
+function getGameOverDisplayInfo(game) {
+  return {
+    gameResultString: `${game.winner} has won!`,
+    playerOneName: game.playerOne,
+    playerTwoName: game.playerTwo,
+    playerOneFleetCoords: game.playerOneFleetCoords,
+    playerTwoFleetCoords: game.playerTwoFleetCoords,
+    playerOneGBHitCoords: game.playerOneGBHitCoords,
+    playerTwoGBHitCoords: game.playerTwoGBHitCoords,
+    playerOneGBMissCoords: game.playerOneGBMissCoords,
+    playerTwoGBMissCoords: game.playerTwoGBMissCoords,
+  };
+}
+
+function getRoundDisplayInfo(game, roundResult, attackCoord) {
+  let resultString;
+  if (roundResult != null) {
+    if (roundResult.isShipSunk) {
+      resultString = `${attackCoord} Attacked!\n${roundResult.attackedPlayer}'s ship was sunk!`;
+    } else if (roundResult.isAHit) {
+      resultString = `${attackCoord} Attacked!\n ${roundResult.attackedPlayer}'s ship was hit!`;
+    } else
+      resultString = `${attackCoord} Attacked!\n ${roundResult.attackingPlayer} missed!`;
+  }
+  return {
+    lastMoveResultString: resultString ?? "This is war!\n ",
+    currentPlayerName: game.currentPlayer,
+    currentPlayerFleetCoords: game.currentPlayerFleetCoords,
+    currentPlayerGBHitCoords: game.currentPlayerGBHitCoords,
+    currentPlayerGBMissCoords: game.currentPlayerGBMissCoords,
+    opposingPlayerName: game.opposingPlayer,
+    opposingPlayerGBMissCoords: game.opposingPlayerGBMissCoords,
+    opposingPlayerGBHitCoords: game.opposingPlayerGBHitCoords,
+  };
+}
+
 function buildGridCellClickFunc(game) {
   return function gridCellClickFunc(event) {
     const attackCoordsString = event.target.id.slice(-2);
@@ -13,43 +49,18 @@ function buildGridCellClickFunc(game) {
     const roundResult = game.playRound(attackCoord);
 
     if (game.isOver) {
-      const gameOverDisplayInfo = {
-        gameResultString: `${game.winner} has won!`,
-        playerOneName: game.playerOne,
-        playerTwoName: game.playerTwo,
-        playerOneFleetCoords: game.playerOneFleetCoords,
-        playerTwoFleetCoords: game.playerTwoFleetCoords,
-        playerOneGBHitCoords: game.playerOneGBHitCoords,
-        playerTwoGBHitCoords: game.playerTwoGBHitCoords,
-        playerOneGBMissCoords: game.playerOneGBMissCoords,
-        playerTwoGBMissCoords: game.playerTwoGBMissCoords,
-      };
+      const gameOverDisplayInfo = getGameOverDisplayInfo(game);
       VC.displayGameOverView(gameOverDisplayInfo);
-      return;
+    } else {
+      const roundDisplayInfo = getRoundDisplayInfo(
+        game,
+        roundResult,
+        attackCoord
+      );
+      if (game.opposingPlayer !== "Skynet")
+        VC.displayTurnTransitionModal(roundDisplayInfo.lastMoveResultString);
+      VC.displayPlayRoundView(roundDisplayInfo, gridCellClickFunc);
     }
-
-    let resultString;
-    if (roundResult.isShipSunk) {
-      resultString = `${attackCoord} Attacked!\n${roundResult.attackedPlayer}'s ship was sunk!`;
-    } else if (roundResult.isAHit) {
-      resultString = `${attackCoord} Attacked!\n ${roundResult.attackedPlayer}'s ship was hit!`;
-    } else
-      resultString = `${attackCoord} Attacked!\n ${roundResult.attackingPlayer} missed!`;
-    const roundDisplayInfo = {
-      lastMoveResultString: resultString,
-      currentPlayerName: game.currentPlayer,
-      currentPlayerFleetCoords: game.currentPlayerFleetCoords,
-      currentPlayerGBHitCoords: game.currentPlayerGBHitCoords,
-      currentPlayerGBMissCoords: game.currentPlayerGBMissCoords,
-      opposingPlayerName: game.opposingPlayer,
-      opposingPlayerGBMissCoords: game.opposingPlayerGBMissCoords,
-      opposingPlayerGBHitCoords: game.opposingPlayerGBHitCoords,
-    };
-
-    if (game.opposingPlayer !== "Skynet")
-      VC.displayTurnTransitionModal(resultString);
-
-    VC.displayPlayRoundView(roundDisplayInfo, gridCellClickFunc);
   };
 }
 
@@ -57,35 +68,13 @@ function startBtnClickFunc(playerNames) {
   const game = new Game(playerNames);
 
   if (game.isOver) {
-    const gameOverDisplayInfo = {
-      gameResultString: `${game.winner} has won!`,
-      playerOneName: game.playerOne,
-      playerTwoName: game.playerTwo,
-      playerOneFleetCoords: game.playerOneFleetCoords,
-      playerTwoFleetCoords: game.playerTwoFleetCoords,
-      playerOneGBHitCoords: game.playerOneGBHitCoords,
-      playerTwoGBHitCoords: game.playerTwoGBHitCoords,
-      playerOneGBMissCoords: game.playerOneGBMissCoords,
-      playerTwoGBMissCoords: game.playerTwoGBMissCoords,
-    };
+    const gameOverDisplayInfo = getGameOverDisplayInfo(game);
     VC.displayGameOverView(gameOverDisplayInfo);
-    return;
+  } else {
+    const roundDisplayInfo = getRoundDisplayInfo(game);
+    const gridCellClickFunc = buildGridCellClickFunc(game);
+    VC.displayPlayRoundView(roundDisplayInfo, gridCellClickFunc);
   }
-
-  const roundDisplayInfo = {
-    lastMoveResultString: "This is war!\n ",
-    currentPlayerName: game.currentPlayer,
-    currentPlayerFleetCoords: game.currentPlayerFleetCoords,
-    currentPlayerGBHitCoords: game.currentPlayerGBHitCoords,
-    currentPlayerGBMissCoords: game.currentPlayerGBMissCoords,
-    opposingPlayerName: game.opposingPlayer,
-    opposingPlayerGBMissCoords: game.opposingPlayerGBMissCoords,
-    opposingPlayerGBHitCoords: game.opposingPlayerGBHitCoords,
-  };
-
-  const gridCellClickFunc = buildGridCellClickFunc(game);
-
-  VC.displayPlayRoundView(roundDisplayInfo, gridCellClickFunc);
 }
 
 VC.displayGameStartView(startBtnClickFunc);
